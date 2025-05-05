5a2bcb5cf93ae4a1f56b9ec32b72c3ada153f267
Lucas Melo <lucas.melo044@gmail.com>
2025-05-05 11:33:05 -0300

    feat: add Main screen

---
+++ src/screens/main/index.tsx
@@ -0,0 +1,13 @@
+import { SafeAreaView, StatusBar, Text, View } from 'react-native' 
+
+import { styles } from './styles' 
+
+export const Main = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text>Hello, world</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
