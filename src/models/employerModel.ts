04dd8103b79f7a0cba2571af8cda4c83c565c094
Lucas Melo <lucas.melo044@gmail.com>
2025-05-12 15:21:59 -0300

    feat: create EmployerModel

---
+++ src/models/employerModel.ts
@@ -0,0 +1,18 @@
+export const EmployerModel = {
+    name: {
+      title: 'Nome do funcionário',
+      placeholder: 'Maria do Barrio'
+    },
+    employerId: {
+      title: 'Matrícula',
+      placeholder: 'ex: 211314324'
+    },
+    password: {
+      title: 'Senha',
+      placeholder: 'ex: 12431iosnfio123'
+    },
+    passwordConfirmation: {
+      title: 'Senha de confirmação',
+      placeholder: 'ex: 12431iosnfio123'
+    }
+  }
