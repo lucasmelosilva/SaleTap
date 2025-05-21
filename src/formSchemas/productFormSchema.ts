24d3375d1fb4f409b013a51eb0f1c007e0ac43b6
Lucas Melo <lucas.melo044@gmail.com>
2025-05-21 18:54:19 -0300

    feat: add ProductFormSchema for product form field config

---
+++ src/formSchemas/productFormSchema.ts
@@ -0,0 +1,18 @@
+export const ProductFormSchema = {
+  name: {
+    title: 'Nome',
+    placeholder: 'Calça Jeans'
+  },
+  barcode: {
+    title: 'Código de barra',
+    placeholder: '1234567343'
+  },
+  image: {
+    title: 'Foto do produto',
+    placeholder: ''
+  },
+  price: {
+    title: 'Preço',
+    placeholder: '70,00'
+  }
+}
