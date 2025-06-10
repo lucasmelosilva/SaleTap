d9ff4a713fca55f43346bc43a1e66cbc3f5f2b67
lucas melo <lucas.melo044@gmail.com>
2025-06-10 20:56:21 -0300

    feat: add Scanner component with barcode reading by camera and integrated input

---
+++ src/components/input/barcode/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+   content: {
+    gap: 20
+  },
+  contentLabel: {
+    fontSize: 15,
+    fontWeight: 'semibold'
+  },
+  
+  input: {
+    padding: 15,
+    fontSize: 18,
+    backgroundColor: '#F1F1F1',
+    color: '#000',
+    borderRadius: 10,
+  },
+})
